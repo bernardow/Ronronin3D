a8dd97617efbf973ef2c91f85c0d76ce59c378f1
bernardow <bernardoritzel@gmail.com>
2023-09-06 10:32:17 -0300

    squash-me

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,7 +9,32 @@ namespace Managers
     {
         [SerializeField] private GameObject _optionsScreen;
         [SerializeField] private GameObject _menuScreen;
+        [SerializeField] private RectTransform _pointer;
         
+        private Dictionary<int, Vector3> selectorDictionary = new Dictionary<int, Vector3>();
+        private int _indexer;
+        
+        private void Start()
+        {
+            selectorDictionary.Add(0, new Vector3(0, 205, 0));
+            selectorDictionary.Add(1, Vector3.zero);
+            selectorDictionary.Add(2, new Vector3(0, -205, 0));
+        }
+
+        private void Update()
+        {
+            _pointer.position = selectorDictionary[_indexer];
+            CheckForArrowUpdate();
+        }
+
+        private void CheckForArrowUpdate()
+        {
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                _indexer++;
+            }
+        }
+
         public void ChangeScene(int index) => SceneManager.LoadScene(index);
 
         public void DisplayOptions()
